86ceb6fb3f092df662af0b377d14ed9f6ab99847
SupremeEvilGod <maheshpk@duck.com>
2024-01-27 22:40:23 +0530

    added a favicon,created changes in track css and html, also made changes in app.js

--- app.js
+++ app.js
@@ -35,3 +35,22 @@ thumbnails.forEach((thumbnail, index) => {
         showSlider();
     })
 })
+
+function page4Animation() {
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
+    })
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
